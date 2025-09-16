db2c435bcfb8cc4ed53e86f030cc01dc2526a941
Israel Azafe <Israel.azafe@gmail.com>
2025-09-16 12:57:33 -0300

    Versão limpa do portal-eventos

--- Area de Trablho/Backup/PROJETOS/1_exercicio_01/scripts.js
+++
@@ -1,3 +0,0 @@
-console.log("Olá, meu nome é Israel Azafe")
-console.log('estou com fome!')
-console.log(`Eu tenho ${25} anos e ${1.74} de altura.`)
--- Area de Trablho/Backup/PROJETOS/2_exercicio_02/scripts.js
+++
@@ -1,3 +0,0 @@
-console.log(877)
-console.log(17.6)
-console.log(6 * 12 / 9)
